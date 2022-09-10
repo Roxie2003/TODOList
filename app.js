4ac4bf0fff020c3088860202334f68e7d8231059
Roxie2003 <59964427+Roxie2003@users.noreply.github.com>
2022-09-10 18:44:20 +0530

    Added about page with separate College Tasks list

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"))
 let items=['Buy food','Cook food','Eat food'];
-
+let clgItems=[];
 app.get("/",(req,res)=>{
     let today=new Date();
      let options={
@@ -16,16 +16,34 @@ app.get("/",(req,res)=>{
 
      let day=today.toLocaleDateString("en-US",options);
 
-     res.render("list",{
-        kindOfDay:day,
-        items
+     res.render("list",{listTitle:day, items
      });
 });
 
 app.post("/",(req,res)=>{
-    items.push(req.body.newItem);
-    res.redirect("/");
+    let list=req.body.list;
+    console.log(list);
+    let item=req.body.newItem;
+    if(list==="College")
+    {
+        clgItems.push(item);
+        res.redirect("/College");
+    }    
+    else
+    {
+        items.push(item);
+        res.redirect("/");
+    }    
+    
 });
+
+app.get("/College",(req,res)=>{
+    res.render("list",{listTitle:"College Tasks",items:clgItems});
+})
+
+app.get("/About",(req,res)=>{
+    res.render("about");
+})
 
 app.listen(3000,function(){
     console.log("Server running!!!")
